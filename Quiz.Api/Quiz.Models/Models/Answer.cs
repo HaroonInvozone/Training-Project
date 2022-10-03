1541c9171464a35d45ca23e9e08b3af1baa67d78
haroonInvozone <haroon.ali@invozone.com>
2022-10-03 19:34:16 +0500

    make models of question and answers

---
+++ Quiz.Api/Quiz.Models/Models/Answer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Models.Models
+{
+    public class Answer
+    {
+        public Guid Id { get; set; }
+        public string Option { get; set; }
+        public bool IsCorrect { get; set; }
+        public Guid QuestionId { get; set; }
+        public Question question { get; set; }
+    }
+}
---
+++ Quiz.Api/Quiz.Models/Models/Question.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Quiz.Models.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
